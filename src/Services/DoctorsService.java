59397af51775f3e83388efb105bde4932f27d9ae
JoseBernabe123 <00038121@uca.edu.sv>
2025-04-06 13:36:31 -0600

    Buscar doctor por su codigo

--- src/Services/DoctorsService.java
+++ src/Services/DoctorsService.java
@@ -1,38 +1,40 @@
 package Services;
 
 import Models.Doctor;
-
 import java.util.HashSet;
 import java.util.List;
 
 public class DoctorsService {
     private final HashSet<Doctor> doctors = new HashSet<>();
 
-    public DoctorsService() {}
-
-    public List<Doctor> getDoctors()
-    {
+    public List<Doctor> getDoctors() {
         return doctors.stream().map(Doctor::clone).toList();
     }
 
-    public boolean add(Doctor doctor)
-    {
+    public boolean add(Doctor doctor) {
         return doctors.add(doctor.clone());
     }
 
-    public boolean update(Doctor doctor)
-    {
-        if(!doctors.contains(doctor)) return false;
+    public boolean update(Doctor doctor) {
+        if (!doctors.contains(doctor)) return false;
 
         doctors.remove(doctor);
         doctors.add(doctor.clone());
         return true;
-
     }
 
-    public boolean remove(Doctor doctor)
-    {
-        doctors.remove(doctor);
-        return true;
+    public boolean remove(Doctor doctor) {
+        return doctors.remove(doctor);
+    }
+
+    //  Codigo para buscar un doctor por su código
+    public Doctor findByCode(String code) {
+        for (Doctor d : getDoctors()) {
+            if (d.getCode().toString().equalsIgnoreCase(code)) {
+
+                return d.clone();
+            }
+        }
+        return null;
     }
 }
