4c53f0dabce3742ccfea8be204413891f2083796
JoseBernabe123 <00038121@uca.edu.sv>
2025-04-06 14:07:09 -0600

    Correccion sobre agregar un doctor

--- src/Models/Doctor.java
+++ src/Models/Doctor.java
@@ -1,70 +1,130 @@
 package Models;
 
 import java.time.LocalDate;
+import java.util.Objects;
+import java.util.UUID;
 
-public class Doctor extends Person implements Cloneable{
+public class Doctor implements Cloneable {
+    private final UUID id = UUID.randomUUID(); // ID único que se genera automaticamente
+    private EpicCode code;
+    private String firstName;
+    private String lastName;
+    private String dui;
+    private LocalDate birthDate;
+    private LocalDate recruitedDate;
+    private String specialty;
 
-    protected EpicCode code;
-    protected LocalDate hireDate;
-    protected String specialty;
-
-    public Doctor(String firstName,
-                  String lastName,
-                  LocalDate birthDate,
-                  String dui,
-                  EpicCode code,
-                  LocalDate hireDate,
-                  String specialty) {
-        super(firstName, lastName, birthDate, dui);
+    // Datos del doctor
+    public Doctor(EpicCode code, String firstName, String lastName, String dui, LocalDate birthDate, LocalDate recruitedDate, String specialty) {
         this.code = code;
-        this.hireDate = hireDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dui = dui;
+        this.birthDate = birthDate;
+        this.recruitedDate = recruitedDate;
         this.specialty = specialty;
+    }
+
+    // Getters
+    public UUID getId() {
+        return id;
     }
 
     public EpicCode getCode() {
         return code;
     }
 
-    public LocalDate getHireDate() {
-        return hireDate;
+    public void setCode(EpicCode code) {
+        this.code = code;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDui() {
+        return dui;
+    }
+
+    public void setDui(String dui) {
+        this.dui = dui;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRecruitedDate() {
+        return recruitedDate;
+    }
+
+    public void setRecruitedDate(LocalDate recruitedDate) {
+        this.recruitedDate = recruitedDate;
     }
 
     public String getSpecialty() {
         return specialty;
     }
 
-    @Override
-    public final boolean equals(Object o) {
-        if (!(o instanceof Doctor doctor)) return false;
-//        if (!super.equals(o)) return false;
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
 
-        return getCode().equals(doctor.getCode());
+    @Override
+    public Doctor clone() {
+        try {
+            Doctor clone = (Doctor) super.clone();
+            clone.code = code;
+            clone.firstName = firstName;
+            clone.lastName = lastName;
+            clone.dui = dui;
+            clone.birthDate = birthDate;
+            clone.recruitedDate = recruitedDate;
+            clone.specialty = specialty;
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Doctor that)) return false;
+        return Objects.equals(getCode(), that.getCode());
     }
 
     @Override
     public int hashCode() {
-        return code.hashCode();
+        return Objects.hash(getCode());
     }
 
     @Override
     public String toString() {
         return "Doctor{" +
-                "code=" + code +
+                "id=" + id +
+                ", code=" + code +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", specialty='" + specialty + '\'' +
-                ", hireDate=" + hireDate +
                 ", dui='" + dui + '\'' +
                 ", birthDate=" + birthDate +
+                ", recruitedDate=" + recruitedDate +
+                ", specialty='" + specialty + '\'' +
                 '}';
-    }
-
-    @Override
-    public Doctor clone() {
-        Doctor clone = (Doctor) super.clone();
-        clone.code = code.clone();
-        clone.hireDate = hireDate;
-        clone.specialty = specialty;
-        return clone;
     }
 }
