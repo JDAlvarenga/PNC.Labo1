e47b96bd2ef2394eca37aa3f46c4dcfe00076607
JoseBernabe123 <00038121@uca.edu.sv>
2025-04-06 13:42:36 -0600

    Buscar un paciente por su DUI

--- src/Services/PatientsService.java
+++ src/Services/PatientsService.java
@@ -10,29 +10,34 @@ public class PatientsService {
 
     public PatientsService() {}
 
-    public List<Patient> getPatients()
-    {
+    public List<Patient> getPatients() {
         return patients.stream().map(Patient::clone).toList();
     }
 
-    public boolean add(Patient patient)
-    {
+    public boolean add(Patient patient) {
         return patients.add(patient.clone());
     }
 
-    public boolean update(Patient patient)
-    {
-        if(!patients.contains(patient)) return false;
+    public boolean update(Patient patient) {
+        if (!patients.contains(patient)) return false;
 
         patients.remove(patient);
         patients.add(patient.clone());
         return true;
-
     }
 
-    public boolean remove(Patient patient)
-    {
+    public boolean remove(Patient patient) {
         patients.remove(patient);
         return true;
+    }
+
+    // Codigo para buscar un paciente por su DUI
+    public Patient findByDui(String dui) {
+        for (Patient p : getPatients()) {
+            if (p.getDui().equalsIgnoreCase(dui)) {
+                return p.clone();
+            }
+        }
+        return null;
     }
 }
